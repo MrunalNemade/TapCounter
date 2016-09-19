223496e611a9c7455bd03acec2b4171bd5a05a75
Mrunalini <mrunalini@Mrunalinis-Mac-Pro.local>
2016-09-19 10:38:45 +0530

    Initial Commit

---
+++ MNTapCounter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MNTapCounter
+//
+//  Created by Mrunalini on 19/09/16.
+//  Copyright © 2016 Mrunalini Nemade. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MNTapCounter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MNTapCounter
+//
+//  Created by Mrunalini on 19/09/16.
+//  Copyright © 2016 Mrunalini Nemade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
